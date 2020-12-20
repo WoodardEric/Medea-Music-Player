59aee3b6fda78894c0aacf90e8b9796a07d347e7
WoodyEric <woodard.eric09@gmail.com>
2020-12-19 20:15:36 -0800

    Remove unused files

--- MiddlePanel.cpp
+++
@@ -1,8 +0,0 @@
-#include "MiddlePanel.h"
-
-MiddlePanel::MiddlePanel(wxWindow *parent)
-	: wxPanel(parent, wxID_ANY)
-{
-	mList = new wxListView(this);
-	mList->SetBackgroundColour(wxColour(179, 198, 253));
-}
